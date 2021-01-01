4c042a3876bae4a0298f67f426aee67ea139482d
Dmitry_Neustupov <neustupov@yandex.ru>
2021-01-01 14:26:26 +0300

    composition pattern - дефолтные методы в интерфейсе компонентов, javadoc

--- src/elements/Glyph.java
+++ src/elements/Glyph.java
@@ -3,11 +3,70 @@ package elements;
 import display.Window;
 import position.Point;
 
+/**
+ * Родительский интерфейс элементов
+ */
 interface Glyph {
 
-  void draw(Window window);
+  /**
+   * Отрисовывает компонент
+   *
+   * @param window Window
+   */
+  default void draw(Window window) {
+    throw new UnsupportedOperationException();
+  }
 
-  boolean intersects(Point point);
+  /**
+   * Границы
+   *
+   * @param glyph Объект компонента
+   */
+  default void bounds(Glyph glyph) {
+    throw new UnsupportedOperationException();
+  }
 
-  Glyph parent();
+  /**
+   * Пересечения компонентов
+   *
+   * @param point Point
+   */
+  default boolean intersects(Point point) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Добавить дочерний компонент по индексу
+   *
+   * @param glyph Объект компонента
+   * @param i Индекс
+   */
+  default void insert(Glyph glyph, int i) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Удалить дочерний компонент
+   *
+   * @param glyph Объект компонента
+   */
+  default void remove(Glyph glyph) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Возвращает дочерний компонент
+   *
+   * @param i Индекс
+   */
+  default Glyph child(int i) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Возвращает родителя компонента
+   */
+  default Glyph parent() {
+    throw new UnsupportedOperationException();
+  }
 }
--- src/elements/Rectangle.java
+++ src/elements/Rectangle.java
@@ -3,10 +3,19 @@ package elements;
 import display.Window;
 import position.Point;
 
+/**
+ * Конечный компонент прямоугольника
+ */
 public class Rectangle implements Glyph {
 
+  /**
+   * Координаты
+   */
   private int x0, y0, x1, y1;
 
+  /**
+   * Родитель
+   */
   private Glyph parent;
 
   public Rectangle(int x0, int y0, int x1, int y1) {
@@ -16,16 +25,29 @@ public class Rectangle implements Glyph {
     this.y1 = y1;
   }
 
+  /**
+   * Отрисовывает компонент
+   *
+   * @param window Window
+   */
   @Override
   public void draw(Window window) {
     window.drawRectangle(x0, y0, x1, y1);
   }
 
+  /**
+   * Пересечения компонентов
+   *
+   * @param point Point
+   */
   @Override
   public boolean intersects(Point point) {
     return false;
   }
 
+  /**
+   * Возвращает родителя компонента
+   */
   @Override
   public Glyph parent() {
     return parent;
